8c3dd44f9cd2c70e45ffafe4fc700e72ff14120b
caker456mail <caker456mail@gmail.com>
2026-09-13 18:33:47 +0900

    localhost로 되어있는거 수정
    vercel에서는vercel.json 타고 백엔드로감

--- moviewebsite/src/service/MovieService.ts
+++ moviewebsite/src/service/MovieService.ts
@@ -14,7 +14,7 @@ export interface BackendMovie {
 
 // MainPage에서 사용하는 MovieType 형태로 변환
 export const getMovies = async (): Promise<BackendMovie[]> => {
-  const response = await fetch("http://localhost:8080/movies.do"); // 백엔드 포트에 맞게 조정
+  const response = await fetch("/movies.do"); // 백엔드 포트에 맞게 조정
   if (!response.ok) {
     throw new Error(`영화 목록 조회 실패: ${response.status}`);
   }
